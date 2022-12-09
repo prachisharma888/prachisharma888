dcacb8731122b38fbc8c832c19f09fa23ae3ba47
prachi sharma <120013988+prachisharma888@users.noreply.github.com>
2022-12-09 17:46:31 +0530

    Add files via upload

---
+++ labtest/src/labtest/GreatestNumber.java
@@ -0,0 +1,27 @@
+package labtest;
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double n1 = -9, n2 = 4, n3 = 9;
+
+        if(n1 >= n2)
+
+    {
+            if(n1 >= n3)
+                System.out.println(n1 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        }
+            else 
+
+{
+            if(n2 >= n3)
+                System.out.println(n2 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        }
+	}
+
+}
