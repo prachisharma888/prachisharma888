4854d1da7a97e21605859a20c701ec5faf9ab7ba
prachi sharma <120013988+prachisharma888@users.noreply.github.com>
2023-02-02 17:24:35 +0530

    Add files via upload

---
+++ labtest6/Collection.java
@@ -0,0 +1,14 @@
+package labtest6;
+
+import java.util.*;
+public class Collection {
+  public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add("Orange");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  System.out.println(list_Strings);
+ }
+}
---
+++ labtest6/Example2.java
@@ -0,0 +1,18 @@
+package labtest6;
+
+import java.util.*;
+  public class Example2 {
+  public static void main(String[] args) {
+         // Create a empty hash set
+     HashSet<String> h_set = new HashSet<String>();
+   // use add() method to add values in the hash set
+          h_set.add("Red");
+          h_set.add("Green");
+          h_set.add("Black");
+          h_set.add("White");
+          h_set.add("Pink");
+          h_set.add("Yellow");
+    System.out.println("Original Hash Set: " + h_set);
+    System.out.println("Size of the Hash Set: " + h_set.size());
+   }
+}
